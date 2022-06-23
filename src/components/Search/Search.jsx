4e0ae88ff5a06b1862507573728248fc477b650c
Jean-Luc LaCosse <jeanlacosse@gmail.com>
2022-06-23 13:33:42 -0500

    craeteing giphy api

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,6 +1,10 @@
 function Search () {
     return (
         <>
+        {/* create search bar
+        send search to API as params
+        add result to state 
+        Display state of 5 gifs */}
         </>
     )
 }
