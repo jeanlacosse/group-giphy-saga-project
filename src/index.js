eaa566987b5fbfc819f86778d2a912247db5cd97
John Maras <sportsballenthusiast@gmail.com>
2022-06-23 13:49:56 -0500

    favorite router and get are wired up with the reducer created

--- src/index.js
+++ src/index.js
@@ -8,16 +8,44 @@ import createSagaMiddleware from 'redux-saga';
 // import { takeEvery, put } from 'redux-saga/effects';
 import logger from 'redux-logger'
 
+const favoriteList = (state = [], action) => {
+  switch (action.type) {
+    case 'SET_FAVORITES':
+      return action.payload
+    default:
+        return state
+  }
+}
+
+function* fetchFavorites(action){
+  console.log('rootSaga', action)
+  let res;
+  try{
+    res = yield axios.get('/api/favorite');
+    console.log('res.data in fetch', res.data)
+  }
+  catch (err) {
+    alert('GET in fetch')
+    console.log(err)
+    return;
+  }
+  // you can't dispatch in your store, so you use a put
+  yield put({
+    //sending the data back to the reducer to set the state
+    type: 'SET_FAVORITES',
+    payload: res.data
+  })
+}
 
 function* watcherSaga() {
-    
+    yield takeEvery('FETCH_FAVORITES', fetchFavorites)
   }
 
 const sagaMiddleware = createSagaMiddleware()
 
 const store = createStore(
     combineReducers({ 
-        
+        favoriteList
      }),
     applyMiddleware(sagaMiddleware, logger)
   );
