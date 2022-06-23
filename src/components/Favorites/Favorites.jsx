bf9aa24ce39b2281cf86cb7902cd26e1463466e8
John Maras <sportsballenthusiast@gmail.com>
2022-06-23 14:12:29 -0500

    started creating what would append from the favorites

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,7 +1,34 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux'
+
 function Favorites () {
+    const dispatch = useDispatch()
+    const favoriteList = useSelector(store => store.favoriteList)
+
+    useEffect(() => {
+        fetchFavorites()
+    }, []);
+
+    const fetchFavorites = () => {
+        dispatch({
+            type: 'FETCH_FAVORITES'
+        })
+    }
+
     return (
-        <>
-        </>
+        <div>
+            <h3>FAVORITES LIST</h3>
+            <ul>
+                {favoriteList.map(favorite => (
+                    <li
+                    key={favorite.id}
+                    url={favorite.url}
+                    >
+                        {favorite.data && <img src={favorite.data.images.original.url} />}
+                    </li>
+                ))}
+            </ul>
+        </div>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
-// import { takeEvery, put } from 'redux-saga/effects';
+import { takeEvery, put } from 'redux-saga/effects';
 import logger from 'redux-logger'
 
 const favoriteList = (state = [], action) => {
@@ -53,4 +53,4 @@ const store = createStore(
   // run the saga middleware
   sagaMiddleware.run(watcherSaga);
 
-  ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('react-root'));
+  ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
